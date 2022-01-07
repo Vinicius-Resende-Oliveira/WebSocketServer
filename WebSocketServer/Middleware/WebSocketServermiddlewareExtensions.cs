57d9c625f098be34ced9463ef6134b41fca279f1
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2022-01-06 23:20:42 -0300

    Adicionar arquivos de projeto.

---
+++ WebSocketServer/Middleware/WebSocketServermiddlewareExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebSocketServer.Middleware
+{
+    public static class WebSocketServermiddlewareExtensions
+    {
+        public static IApplicationBuilder UseWebSocketServer(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<WebSocketServerMiddleware>();
+        }
+
+        public static IServiceCollection AddWebSocketServerConnectionManager(this IServiceCollection services)
+        {
+            services.AddSingleton<WebSocketServerConnectionManager>();
+            return services;
+        }
+    }
+}
---
+++ WebSocketServer/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Http;
+using WebSocketServer.Middleware;
+
+namespace WebSocketServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebSocketServerConnectionManager();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseWebSockets();
+
+            app.UseWebSocketServer();
+
+            app.Run(async context =>
+            {
+                Console.WriteLine("3nd (terminal) Request Delegate");
+                await context.Response.WriteAsync("3rd (terminal) Request Delegate");
+            });
+        }
+    }
+}
